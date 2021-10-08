b09384a76dca2978c11702c7a613c4b9dec17fef
aditprasetio <aditprasetio16@gmail.com>
2021-10-08 21:41:23 +0700

    Problem 2
     lingkaran bergerak dengan suatu kecepatan konstan sehingga keluar dari layar

---
+++ problem-2/Assets/Script/PlayerMovment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D player_rigidbody;
+
+    private void Awake()
+    {
+        player_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        player_rigidbody.AddForce(new Vector2(1, 1) * moveSpeed);
+    }
+}
